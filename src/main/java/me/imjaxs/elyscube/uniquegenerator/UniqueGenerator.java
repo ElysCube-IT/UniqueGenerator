16701fb846826ae34f53bf94031b4c7266558140
ImJaxs <gentile.christian@outlook.it>
2022-07-28 03:35:58 +0200

    Initial Commit

---
+++ src/main/java/me/imjaxs/elyscube/uniquegenerator/UniqueGenerator.java
@@ -0,0 +1,17 @@
+package me.imjaxs.elyscube.uniquegenerator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UniqueGenerator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
